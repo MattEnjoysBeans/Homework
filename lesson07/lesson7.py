ae53604e815d89309401ee35269593c9271c86c5
MattEnjoysBeans <mdromeo2014@gmail.com>
2025-09-23 09:53:13 -0400

    completed lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,10 @@
 # Lesson 7
+import random
+# input
+diff_class = int(input("Enter the Difficulty Number: "))
+ability_num = random.randrange(1, 20)
+
+if ability_num >= diff_class:
+    print(f"Success (Rolled a {ability_num})")
+else:
+    print(f"Not powerful enough... (Rolled a {ability_num})")
