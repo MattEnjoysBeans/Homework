3d5c69b20ddaf5f764cd2981e1bdd7d1af1ef536
MattEnjoysBeans <mdromeo2014@gmail.com>
2025-09-23 10:54:11 -0400

    completed lesson 8 and 9

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,17 @@
 # Lesson 8
+# input
+win_count = 0
+for i in range(6):
+    is_win = input("Enter 'W' for win or 'L' for loss: ")
+    is_win = is_win.lower()
+    if (is_win == "w"):
+        win_count += 1
+
+if win_count == 0:
+    print("Player eliminated.")
+elif win_count <= 2:
+    print("Group 3")
+elif win_count <= 4:
+    print("Group 2")
+elif win_count <= 6:
+    print("Group 1")
--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,34 @@
 # Lesson 9
+from random import choice
+# input
+player_choice = ""
+# make sure input is valid
+while True:
+    player_choice = input("Enter 'Rock' 'Paper' or 'Scissors': ")
+    player_choice = player_choice.lower()
+    if player_choice in {"rock", "paper", "scissors"}:
+        break
+
+cpu_choice = choice(["rock", "paper", "scissors"])
+print(f"CPU Chose: {cpu_choice}")
+
+if player_choice == cpu_choice:
+    print("Tie")
+else:
+    match player_choice:
+        case "rock":
+            if cpu_choice == "paper":
+                print("CPU wins")
+            else:
+                print("Player wins")
+        case "paper":
+            if cpu_choice == "scissors":
+                print("CPU wins")
+            else:
+                print("Player wins")
+        case _:
+            if cpu_choice == "rock":
+                print("CPU wins")
+            else:
+                print("Player wins")
+
